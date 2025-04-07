e7b520503ca835891cbfe286d5d01d1a09e8fb95
Isabella334 <isabellarecinos@gmail.com>
2025-04-07 14:19:05 -0600

    DAPA 24 | Conexión entre backend y frontend

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const respuesta = ref('Cargando...')
+
+onMounted(async () => {
+  try {
+    const res = await fetch(`${import.meta.env.BASE_URL}/ping`)
+    const data = await res.json()
+    respuesta.value = data.message
+  } catch (error) {
+    respuesta.value = 'Error al conectar con el backend'
+    console.error(error)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h2>Verificación de conexión con el backend:</h2>
+    <p>{{ respuesta }}</p>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: #2c3e50;
+}
+</style>
