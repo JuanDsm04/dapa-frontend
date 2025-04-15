76c4b7d2be503e1bc6511714debea14d95850366
nilsmuralles <murallesnils11@gmail.com>
2025-04-14 23:11:03 -0600

    DAPA 40 | feat: Added Login Form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,96 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const email = ref('')
+  const password = ref('')
+  const showPassword = ref(false)
+
+  const togglePassVisibility = () => {
+    showPassword.value = !showPassword.value
+  }
+</script>
+
+<template>
+  <main>
+    <form @submit.prevent>
+      <div class="field">
+        <label for="Email">Email</label>
+        <input type="text" id="Email" name="Email" v-model="email">
+      </div>
+      <div class="field">
+        <div class="pass-row">
+          <label for="Password">Contraseña</label>
+          <button type="button" class="toggle-visibility-btn" @click="togglePassVisibility">
+            <span class="icon">o</span> 
+            <span class="toggle-label">{{ showPassword ? 'Ocultar' : 'Ver' }}</span>
+          </button>
+        </div>
+        <input type="password" id="Password" name="Password" v-model="password">
+      </div>
+      <button type="submit">Iniciar sesión</button>
+      <a href="">¿Olvidaste tu contraseña?</a>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 450px;
+  }
+
+  form, .field {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  form {
+    gap: 1.5rem;
+  }
+
+  label {
+    font-size: 0.95rem;
+    margin-bottom: 0.1rem;
+  }
+
+  .pass-row {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  input {
+    padding: 0.75rem 1rem;
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    font-size: 1rem;
+  }
+
+  button {
+    border: 0;
+    padding: 15px 0;
+    border-radius: 32px;
+    background-color: var(--color-form-button);
+    font-size: 1.10rem;
+    font-weight: 500;
+  }
+
+  .toggle-visibility-btn {
+    display: flex;
+    gap: 10px;
+    color: gray;
+    align-items: center;
+    margin-right: 10px;
+    background-color: transparent;
+    padding: 0;
+  }
+
+  .toggle-label {
+    font-weight: lighter;
+    font-size: 0.90rem;
+  }
+
+  a {
+    color: black;
+    align-self: center;
+  }
+</style>
