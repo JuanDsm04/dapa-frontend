5636bb7dcaa8ca9aeb7d9f9d4d7c0e0fa93c46b6
Diego Flores <floresdiego041@gmail.com>
2025-04-17 22:08:58 -0600

    DAPA-44 | feat: save token in local storage

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -28,6 +28,8 @@
 
       const data = await response.json()
       console.log(data)
+      //Guardar el token en localStorage
+      localStorage.setItem(token, data.data)
 
     } catch (e) {
       console.log(e)
