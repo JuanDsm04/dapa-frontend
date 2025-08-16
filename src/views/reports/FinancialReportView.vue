2195b3fe2b4ef14fd03b0add26ef31b4b33b6343
nilsmuralles <murallesnils11@gmail.com>
2025-08-15 22:07:29 -0600

    DAPA 204 | feat: Add main component for viewing editing and deleting

--- src/views/reports/FinancialReportView.vue
+++ src/views/reports/FinancialReportView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import ReportFilter from '@/components/filters/ReportFilter.vue';
+import FinanceTable from '@/components/Table.vue'
 </script>
 
 <template>
   <main>
     <ReportFilter />
+    <FinanceTable
+      :items="[]"
+      :columns="[
+        { label: 'ID', field: 'id' },
+        { label: 'Fecha', field: 'date' },
+        { label: 'Tipo', field: 'type' },
+        { label: 'Monto (Q)', field: 'amount' },
+        { label: 'Categoría', field: 'category' },
+        { label: 'Método de pago', field: 'paymentMethod' },
+        { label: 'Responsable', field: 'responsable' },
+        { label: 'Descripción', field: 'desc' },
+      ]"
+      @edit="() => {}"
+      @delete="() => {}"
+    />
+
   </main>
 </template>
