488f9e6ad89c8e3f32952beb32d9558aef505558
Isabella334 <isabellarecinos@gmail.com>
2025-08-17 23:44:16 -0600

    DAPA-191 | fix: ReportsView.vue

--- src/views/reports/ReportsView.vue
+++ src/views/reports/ReportsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import BigButton from '@/components/BigButton.vue'
+import BigButton from '@/components/BigButton.vue';
 import router from '@/router';
 
 function goToControl() { }
@@ -13,19 +13,22 @@ function goToPerformance() { }
 
 <template>
   <main class="financial-main">
-    <div class="btn-row">
+    <div class="btn-column">
       <BigButton
         title="Control financiero"
+        description="Administra y visualiza el control de ingresos y egresos."
         image="/src/assets/images/finance-control.png"
         :onClick="goToControl"
       />
       <BigButton
         title="Reporte financiero"
+        description="Consulta reportes detallados sobre el estado financiero."
         image="/src/assets/images/finance-report.png"
         :onClick="goToReport"
       />
       <BigButton
         title="Reporte de desempeño"
+        description="Analiza el desempeño y resultados de actividades."
         image="/src/assets/images/performance.png"
         :onClick="goToPerformance"
       />
@@ -40,10 +43,9 @@ function goToPerformance() { }
   align-items: center;
   height: 100vh;
 }
-.btn-row {
+.btn-column {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
   gap: 40px;
 }
 </style>
